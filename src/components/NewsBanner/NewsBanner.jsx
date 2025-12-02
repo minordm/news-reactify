d935d0c8e10078bfd6de17be9bce5ed568f96dc9
minor.dm <lugovoi21@gmail.com>
2025-12-02 22:59:01 +0500

    refactoring

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,8 +1,11 @@
 import styles from "./styles.module.css";
 import { formatTimeAgo } from "../../helpers/formatTimeAgo";
 import Image from "../Image/Image";
+import withSkeleton from "../../helpers/hocs/withSkeleton";
 
 const NewsBanner = ({ item }) => {
+  console.log(item);
+
   return (
     <div className={styles.banner}>
       <Image image={item?.image} />
@@ -14,4 +17,7 @@ const NewsBanner = ({ item }) => {
   );
 };
 
-export default NewsBanner;
+const NewsBannerWithSkeleton = withSkeleton(NewsBanner, "banner", 1);
+
+export default NewsBannerWithSkeleton;
+// export default NewsBanner;
